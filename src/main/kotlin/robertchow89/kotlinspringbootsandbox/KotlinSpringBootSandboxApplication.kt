2e8763cc9bff8546b765f337f2a6c1c755525247
Robert Chow <rchow@thoughtworks.com>
2021-03-07 15:10:14 +0000

    generate template from start.spring.io
    following spring boot kotlin blog tutorial

---
+++ src/main/kotlin/robertchow89/kotlinspringbootsandbox/KotlinSpringBootSandboxApplication.kt
@@ -0,0 +1,11 @@
+package robertchow89.kotlinspringbootsandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootSandboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootSandboxApplication>(*args)
+}
---
+++ src/test/kotlin/robertchow89/kotlinspringbootsandbox/KotlinSpringBootSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package robertchow89.kotlinspringbootsandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootSandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
